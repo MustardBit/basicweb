f65747f3f7d82d94c302e025e6e73134a2993fcf
MustardBit <50325803+MustardBit@users.noreply.github.com>
2019-10-05 08:17:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,135 +1,139 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
+type middleware func(http.Handler) http.Handler
+type middlewares []middleware
+
+func (mws middlewares) apply(hdlr http.Handler) http.Handler {
+	if len(mws) == 0 {
+		return hdlr
+	}
+	return mws[1:].apply(mws[0](hdlr))
+}
+
+func (c *controller) shutdown(ctx context.Context, server *http.Server) context.Context {
+	ctx, done := context.WithCancel(ctx)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		defer done()
+
+		<-quit
+		signal.Stop(quit)
+		close(quit)
+
+		atomic.StoreInt64(&c.healthy, 0)
+		server.ErrorLog.Printf("Server is shutting down...\n")
+
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			server.ErrorLog.Fatalf("Could not gracefully shutdown the server: %s\n", err)
+		}
+	}()
+
+	return ctx
+}
+
+type controller struct {
+	logger        *log.Logger
+	nextRequestID func() string
+	healthy       int64
+}
+
 func main() {
-	mw := multiWeatherProvider{
-		openWeatherMap{apiKey: "x"},
-		weatherUnderground{apiKey: "x"},
+	listenAddr := ":5000"
+	if len(os.Args) == 2 {
+		listenAddr = os.Args[1]
 	}
 
-	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
-		begin := time.Now()
-		city := strings.SplitN(r.URL.Path, "/", 3)[2]
-		temp, err := mw.temperature(city)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"city": city,
-			"temp": temp,
-			"took": time.Since(begin).String(),
-		})
-	})
-	http.ListenAndServe(":8080", nil)
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Printf("Server is starting...")
+
+	c := &controller{logger: logger, nextRequestID: func() string { return strconv.FormatInt(time.Now().UnixNano(), 36) }}
+	router := http.NewServeMux()
+	router.HandleFunc("/", c.index)
+	router.HandleFunc("/healthz", c.healthz)
+
+	server := &http.Server{
+		Addr:         listenAddr,
+		Handler:      (middlewares{c.tracing, c.logging}).apply(router),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	ctx := c.shutdown(context.Background(), server)
+
+	logger.Printf("Server is ready to handle requests at %q\n", listenAddr)
+	atomic.StoreInt64(&c.healthy, time.Now().UnixNano())
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on %q: %s\n", listenAddr, err)
+	}
+	<-ctx.Done()
+	logger.Printf("Server stopped\n")
 }
 
-type multiWeatherProvider []weatherProvider
-
-type weatherProvider interface {
-	temperature(city string) (float64, error)
+func (c *controller) index(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+	fmt.Fprintf(w, "Hello, World!\n")
 }
 
-func (w multiWeatherProvider) temperature(city string) (float64, error) {
-	// Make a channel for temperatures, and a channel for errors.
-	// Each provider will push a value into only one.
-	temps := make(chan float64, len(w))
-	errs := make(chan error, len(w))
+func (c *controller) healthz(w http.ResponseWriter, req *http.Request) {
+	if h := atomic.LoadInt64(&c.healthy); h == 0 {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		fmt.Fprintf(w, "uptime: %s\n", time.Since(time.Unix(0, h)))
+	}
+}
 
-	// For each provider, spawn a goroutine with an anonymous function.
-	// That function will invoke the temperature method, and forward the response.
-	for _, provider := range w {
-		go func(p weatherProvider) {
-			k, err := p.temperature(city)
-			if err != nil {
-				errs <- err
-				return
+func (c *controller) logging(hdlr http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func(start time.Time) {
+			requestID := w.Header().Get("X-Request-Id")
+			if requestID == "" {
+				requestID = "unknown"
 			}
-			temps <- k
-		}(provider)
-	}
-	sum := 0.0
-	// Collect a temperature or an error from each provider.
-	for i := 0; i < len(w); i++ {
-		select {
-		case temp := <-temps:
-			sum += temp
-		case err := <-errs:
-			return 0, err
+			c.logger.Println(requestID, req.Method, req.URL.Path, req.RemoteAddr, req.UserAgent(), time.Since(start))
+		}(time.Now())
+		hdlr.ServeHTTP(w, req)
+	})
+}
+
+func (c *controller) tracing(hdlr http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		requestID := req.Header.Get("X-Request-Id")
+		if requestID == "" {
+			requestID = c.nextRequestID()
 		}
-	}
-	// Return the average, same as before.
-	return sum / float64(len(w)), nil
+		w.Header().Set("X-Request-Id", requestID)
+		hdlr.ServeHTTP(w, req)
+	})
 }
 
-func temperature(city string, providers ...weatherProvider) (float64, error) {
-	sum := 0.0
-	for _, provider := range providers {
-		k, err := provider.temperature(city)
-		if err != nil {
-			return 0, err
-		}
-		sum += k
-	}
-	return sum / float64(len(providers)), nil
-}
-
-type weatherData struct {
-	Name string `json:"name"`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
-}
-
-type openWeatherMap struct {
-	apiKey string
-}
-
-func (w openWeatherMap) temperature(city string) (float64, error) {
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + w.apiKey + "&q=" + city)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	var d struct {
-		Main struct {
-			Kelvin float64 `json:"temp"`
-		} `json:"main"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return 0, err
-	}
-	log.Printf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
-	return d.Main.Kelvin, nil
-}
-
-type weatherUnderground struct {
-	apiKey string
-}
-
-func (w weatherUnderground) temperature(city string) (float64, error) {
-	resp, err := http.Get("http://api.wunderground.com/api/" + w.apiKey + "/conditions/q/" + city + ".json")
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	var d struct {
-		Observation struct {
-			Celsius float64 `json:"temp_c"`
-		} `json:"current_observation"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return 0, err
-	}
-	kelvin := d.Observation.Celsius + 273.15
-	log.Printf("weatherUnderground: %s: %.2f", city, kelvin)
-	return kelvin, nil
-}
+// main_test.go
+var (
+	_ http.Handler = http.HandlerFunc((&controller{}).index)
+	_ http.Handler = http.HandlerFunc((&controller{}).healthz)
+	_ middleware   = (&controller{}).logging
+	_ middleware   = (&controller{}).tracing
+)
